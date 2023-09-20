e7d59690102f7b5902c82bb6606bea11607a93dd
deeepak-me <deepak.krishnan.e@gmail.com>
2023-09-20 16:29:05 +0530

    feat:data file added

---
+++ src/data/data.js
@@ -0,0 +1,36 @@
+import Revenue from "../images/revenue.svg";
+import Transaction from "../images/transaction.svg";
+import Likes from "../images/thumb.svg";
+import Users from "../images/customer.svg";
+
+export const revenue = {
+  title: "Total Revenues",
+  link: "$2,129,430",
+  rate: "+2.5",
+  image: Revenue,
+  color: "#7FCD93",
+};
+
+export const transaction = {
+  title: "Total Transactions",
+  link: "1,520",
+  rate: "+1.7",
+  image: Transaction,
+  color: "#DEBF85",
+};
+
+export const likes = {
+  title: "Total Likes",
+  link: "9,721",
+  rate: "+1.4",
+  image: Likes,
+  color: "#ECA4A4",
+};
+
+export const users = {
+  title: "Total Users",
+  link: "9,721",
+  rate: "+4.2",
+  image: Users,
+  color: "#A9B0E5",
+};
