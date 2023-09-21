25b60a51d9adb7913be7b4461232234af9fa71e5
deeepak-me <deepak.krishnan.e@gmail.com>
2023-09-21 21:50:26 +0530

    feat: props added

--- src/components/usercard/UserCard.jsx
+++ src/components/usercard/UserCard.jsx
@@ -6,44 +6,44 @@ import YouTube from "../../images/youtube.svg";
 import Mail from "../../images/Mail.svg";
 import Instagram from "../../images/instagram.svg";
 
-const UserCard = ({ currUsers }) => {
+const UserCard = ({ currProfile: user }) => {
   return (
     <div className="userCard">
-      {currUsers.map((user) => (
-        <div className="user">
-          <h2 className="title">{user.name}</h2>
-          <div className="info">
-            <div className="segment">
-              <div className="content">
-                <div className="icon whatsapp">
-                  <img src={WhatsApp} />
-                </div>
-                <h4 className="subtitle">{user.phone}</h4>
+      {/* {currProfile.map((user) => ( */}
+      <div className="user">
+        <h2 className="title">{user.name}</h2>
+        <div className="info">
+          <div className="segment">
+            <div className="content">
+              <div className="icon whatsapp">
+                <img src={WhatsApp} />
               </div>
-              <div className="content">
-                <div className="icon mail">
-                  <img src={Mail} />
-                </div>
-                <h4 className="subtitle">{user.email}</h4>
-              </div>
+              <h4 className="subtitle">{user.phone}</h4>
             </div>
-            <div className="segment">
-              <div className="content">
-                <div className="icon insta">
-                  <img src={Instagram} />
-                </div>
-                <h4 className="subtitle">{user.instagram}</h4>
+            <div className="content">
+              <div className="icon mail">
+                <img src={Mail} />
               </div>
-              <div className="content">
-                <div className="icon youtube">
-                  <img src={YouTube} />
-                </div>
-                <h4 className="subtitle">{user.youtube}</h4>
+              <h4 className="subtitle">{user.email}</h4>
+            </div>
+          </div>
+          <div className="segment">
+            <div className="content">
+              <div className="icon insta">
+                <img src={Instagram} />
               </div>
+              <h4 className="subtitle">{user.instagram}</h4>
+            </div>
+            <div className="content">
+              <div className="icon youtube">
+                <img src={YouTube} />
+              </div>
+              <h4 className="subtitle">{user.youtube}</h4>
             </div>
           </div>
         </div>
-      ))}
+      </div>
+      {/* ))} */}
     </div>
   );
 };
